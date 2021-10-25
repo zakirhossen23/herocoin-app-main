46b0b60ad478940b8940bb8285642c715e71b631
Zakir Hossen <zakirhoss12345@gmail.com>
2021-10-25 06:33:15 +0600

    finished

---
+++ android/app/src/main/java/com/herocoin/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.herocoin.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.herocoin.app',
+  appName: 'herocoin',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
